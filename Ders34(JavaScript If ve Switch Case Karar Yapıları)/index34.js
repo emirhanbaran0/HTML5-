5c2fdb7dbbd4a3c08e1417d72ad6dc0a10776127
Emirhan Baran <emir.baran255@gmail.com>
2023-03-19 20:06:01 +0300

    Create index34.js

---
+++ Ders34(JavaScript If ve Switch Case Karar Yapıları)/index34.js
@@ -0,0 +1,23 @@
+var x=5,y=6;
+
+if(x>y){
+    d ocumment.write("x y'den büyüktür!");
+}
+else if(x<y){
+    documment.write("x y'den küçüktür!");
+}
+else{
+    document.write("Sayıların eşit olma durumu");
+}
+switch(x){
+    case 1:
+        document.write("x=1");
+        break;
+     case 2:
+        document.write("x=2");
+        break
+    default:
+        document.write("X 1 veya 2'ye işt değildir");
+        break;
+
+}
