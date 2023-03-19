59c8f81f99b76823941c36282cf8837ba227ffa0
Emirhan Baran <emir.baran255@gmail.com>
2023-03-19 19:51:50 +0300

    Create index33.js

---
+++ Ders33(JavaScript Mantıksal Operatörler)/index33.js
@@ -0,0 +1,9 @@
+document.write( 3==5 +" <br>" );
+document.write( 3!=5 +" <br>" );
+document.write( 3>5 +"<br>" );
+document.write( 3<5 +"<br>" );
+document.write( 3>=5 +"<br>" );
+document.write( 3<=5 +"<br>" );
+document.write( 3>5  || 8<5 +"<br>" );  // || VEYA OPERATÖRÜ
+document.write( 3>5  && 8<5 +"<br>" );  // || VE OPERATÖRÜ
+document.write(!true );  // || Değil OPERATÖRÜ
